b32c7a8c1965f0e438e4d574d9c892bee76229ae
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-01-05 20:00:00 -0600

    finished project

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
 import smtplib
-
-my_email = "alonzosanchez3@gmail.com"
-
-
-
-# with smtplib.SMTP('smtp.gmail.com') as connection:
-#   connection.starttls()
-#   connection.login(user=my_email, password=password)
-#   connection.sendmail(from_addr=my_email, to_addrs="brookthornock@gmail.com", msg="Subject:Test\n\nI hate you")
-
-import datetime as dt
-
-# now = dt.datetime.now()
-# year = now.year
-# if year == 2023:
-#   print(True)
-# print(now)
-
-# date_of_birth = dt.datetime(year=1995, month=7, day=10)
-# print(date_of_birth)
 import random
+import datetime as dt
+import pandas
 
-now = dt.datetime.now()
-weekday = now.weekday()
-print(weekday)
-if weekday == 6:
-  with open('quotes.txt') as quote_file:
-    all_quotes = quote_file.readlines()
-    quote = random.choice(all_quotes)
-  print(quote)
+MY_EMAIL = 'alonzosanchez3@gmail.com'
+FILES_LIST = ['letter_1.txt', 'letter_2.txt', 'letter_3.txt']
+
+def send_email(name, email):
+  random_file = random.choice(FILES_LIST)
+  random_file = 'letter_templates/' + random_file
+  with open(random_file, 'r') as message:
+    text = message.read()
+    text = text.replace('[NAME]', name)
+    print(text)
+
+
   with smtplib.SMTP('smtp.gmail.com') as connection:
     connection.starttls()
-    connection.login(my_email, password)
-    connection.sendmail(from_addr=my_email, to_addrs=my_email, msg=f"Subject:Quote\n\n{quote}")
+    connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+    connection.sendmail(from_addr=MY_EMAIL, to_addrs=email, msg=f"Subject:Happy Birthday!\n\n{text}")
 
+data = pandas.read_csv('./birthday.csv')
+data_list = data.to_dict(orient='records')
+print(data_list)
+now = dt.datetime.now()
+day = now.day
+month = now.month
+for record in data_list:
+  if record['month'] == month and record['day'] == day:
+    send_email(record['name'], record['email'])
 
 
